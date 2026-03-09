fe34dfdc931d413932912bf56628b3e73475b0cc
Prajwal-Jagadeesh-Guliyavar <pjguliyavar10022004@gmail.com>
2026-03-09 22:36:18 +0530

    Implement Category model, register in admin, and add to post forms

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,18 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Categories"
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
@@ -11,6 +21,10 @@ class Post(models.Model):
     #the above statement will let the post have the date and time when published but it cannot be modified after the creation of the future
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
